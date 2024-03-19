171e3cf1325ea4c4cbd727ad4f09326dcf8b5787
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-03-19 23:43:45 +0100

    .

--- src/widgets/HomeSection/ui/HeroSection/index.tsx
+++ src/widgets/HomeSection/ui/HeroSection/index.tsx
@@ -1,12 +1,25 @@
 import { FC } from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 import Wave from "../../../../../public/images/wave.svg";
 import Laptop from "../../../../../public/images/laptop.png";
 import Image1 from "../../../../../public/images/ani.png";
 
+gsap.registerPlugin(SplitText);
+
 const HeroSection: FC = () => {
+    const mySplitText = new SplitText('.split', {type:"chars"} )
+    const chars = mySplitText.chars
+  useGSAP(() => {
+    gsap.from(chars, {
+        yPercent: 130,
+    });
+  }, []);
+
   return (
     <section>
-      <div className="hero min-h-[calc(100vh-64px)]">
+      <div className="hero min-h-[calc(100vh-64px)]" id="hero">
         <div className="container flex-col lg:flex-row">
           <section className="font-jost w-full text-slate-900">
             <div className="flex justify-between gap-10">
@@ -15,10 +28,10 @@ const HeroSection: FC = () => {
                   <div className="h-[83px] w-[140px]">
                     <img src={Image1} />
                   </div>
-                  <h1 className="pl-7 text-8xl">Analytics</h1>
+                  <h1 className="split pl-7 text-8xl">Analytics</h1>
                 </div>
                 <h1 className="pl-[40px] text-8xl">
-                  that <span className=" text-gray-400">helps</span> you
+                  that <span className="split text-gray-400">helps</span> you
                 </h1>
               </div>
               <div className="mt-2 rounded-full">
@@ -30,13 +43,13 @@ const HeroSection: FC = () => {
               </div>
             </div>
             <div className="ml-32 flex justify-center text-right">
-              <h1 className="text-8xl">shape</h1>
+              <h1 className="split text-8xl">shape</h1>
               <div className="mx-2 mt-8 h-fit w-fit rounded-full bg-yellow-400 p-5 py-6 text-center">
                 <div className="-mt-3 rounded-full object-contain font-bold">
                   <img src={Wave} alt="logo" width={33} height={33} />
                 </div>
               </div>
-              <h1 className=" text-8xl">the future</h1>
+              <h1 className="split text-8xl">the future</h1>
             </div>
           </section>
         </div>
